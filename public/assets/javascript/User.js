d01d16ee56f04167b93bd9a4427c87193cfb67c2
tcalhounjr <thomas@nafasi.co>
2018-05-31 20:59:09 -0400

    changed pacakage.jscon and added User class

---
+++ public/assets/javascript/User.js
@@ -0,0 +1,17 @@
+"use strict";
+
+class User {
+    constructor(email, ) {
+        this.maxguesses = 10;
+        this.numWins = 0;
+        this.numLosses = 0;
+        this.lettersAvail = 0;
+        this.cpuGuess = '';
+        this.guesses = [];
+        this.boardStr = '';
+        this.boardArray = [];
+        this.gameArray = [];
+        this.selectedGame = gameType;
+    }
+
+module.exports = User;
