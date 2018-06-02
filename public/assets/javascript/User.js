3d9b8639201a70f2c864f9896a0dc4873181f9bf
tcalhounjr <thomas@nafasi.co>
2018-06-01 21:23:31 -0400

    added a buyLong function just for testing purposes

--- public/assets/javascript/User.js
+++ public/assets/javascript/User.js
@@ -1,17 +1,21 @@
 "use strict";
 
 class User {
-    constructor(email, ) {
-        this.maxguesses = 10;
-        this.numWins = 0;
-        this.numLosses = 0;
-        this.lettersAvail = 0;
-        this.cpuGuess = '';
-        this.guesses = [];
-        this.boardStr = '';
-        this.boardArray = [];
-        this.gameArray = [];
-        this.selectedGame = gameType;
+    constructor(username, userID) {
+        this.username = username;
+        this.userID = userID;
     }
+
+    buyLong(ticker, quantity, price, acctBalance, portfolio) {
+        
+        let position = price * quantity;
+        if (position > acctBalance) {
+            //flash some message 
+        }
+        portfolio.push(ticker);
+        return portfolio;
+    }
+
+}
 
 module.exports = User;
